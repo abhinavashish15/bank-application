52c5c30ca036da3193511a2b072f68d0690a565b
Abhinav Ashish <100207127+abhinavashish15@users.noreply.github.com>
2024-07-16 18:57:53 +0530

    added react router in AuthForm.tsx

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -21,12 +21,14 @@ import { Input } from "@/components/ui/input";
 import { Divide, Loader2 } from "lucide-react";
 import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
   email: z.string().email(),
 });
 
 const AuthForm = ({ type }: { type: string }) => {
+  const router=useRouter();
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -43,12 +45,32 @@ const AuthForm = ({ type }: { type: string }) => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
+  const onSubmit=async (data: z.infer<typeof formSchema>) => {
     setIsLoading(true);
-    console.log(values);
-    setIsLoading(false);
+    try {
+      // Sign up with Appwrite & create plaid token
+
+      if(type === 'sign-up'){
+        // const userData={
+        //   firstName:data.firstName,
+        // }
+      }
+
+      if(type === 'sign-in'){
+        // const response=await SignIn({
+        //   email:data.email,
+        //   password:data.password,
+        // })
+        // if(response) router.push('/')
+
+      }
+
+
+    } catch (error) {
+      console.log(error);
+    }finally{
+       setIsLoading(false);
+    }
   }
 
   return (
